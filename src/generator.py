e92108aeb8a9775a0e9a9dad4782f7abba715e30
Cilliers9 <tclouwrens@gmail.com>
2026-01-11 19:18:07 +0200

    modified:   src/generator.py modified:   src/main.py

--- src/generator.py
+++ src/generator.py
@@ -1,7 +1,7 @@
 import os
 from markdown_to_htmlnode import markdown_to_htmlnode, extract_header
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, mode="r") as md_file:
         markdown = md_file.read()
@@ -11,13 +11,14 @@ def generate_page(from_path, template_path, dest_path):
     html_string = html_nodes.to_html()
     header = extract_header(markdown)
     html = html_template.replace("{{ Title }}", header).replace("{{ Content }}", html_string)
+    
     dest_dir = os.path.dirname(dest_path)
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
     with open(dest_path, mode="w") as index_file:
         write_result = index_file.write(html)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     if not os.path.isdir(dir_path_content):
         raise Exception("Error: Content directory not pointing to valid directory")
     file_list = os.listdir(dir_path_content)
@@ -25,9 +26,9 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         file_path = os.path.join(dir_path_content, file)
         if os.path.isdir(file_path):
             dest_file_path = os.path.join(dest_dir_path, file)
-            generate_pages_recursive(file_path, template_path, dest_file_path)
+            generate_pages_recursive(file_path, template_path, dest_file_path, basepath)
         if os.path.isfile(file_path):
             file_ext = os.path.splitext(file)
             dest_file_path = os.path.join(dest_dir_path, f"{file_ext[0]}.html")
             if file_ext[1] == ".md":
-                generate_page(file_path, template_path, dest_file_path)
+                generate_page(file_path, template_path, dest_file_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import sys
 from static_to_public import update_public
 from generator import generate_pages_recursive
 from constant import DIR_PATH_CONTENT,DEST_DIR_PATH, TEMPLATE_PATH
@@ -5,7 +6,11 @@ from constant import DIR_PATH_CONTENT,DEST_DIR_PATH, TEMPLATE_PATH
 
 def main():
     
+    if sys.argv[0] == "":
+        basepath = "/"
+    else:
+        basepath = sys.argv[0]
     update_public()
-    generate_pages_recursive(DIR_PATH_CONTENT, TEMPLATE_PATH, DEST_DIR_PATH)
+    generate_pages_recursive(DIR_PATH_CONTENT, TEMPLATE_PATH, DEST_DIR_PATH, basepath)
 
 main()
